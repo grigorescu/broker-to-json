c4ac3ceb09c6fbfe1ece0fbf05508f9dc2ea6944
Vlad Grigorescu <vlad@es.net>
2021-06-30 20:41:49 -0500

    Add fix_ports to deal with Broker returning a native type instead.

--- broker_json/utils.py
+++ broker_json/utils.py
@@ -52,6 +52,15 @@ def get_record_types(type_name):
 
     return data
 
+# Broker returns native objects for Port. This will just give a string.
+def fix_ports(val):
+    try:
+        for i in range(len(val)):
+            val[i] = fix_ports(val[i])
+    except TypeError:
+        if isinstance(val, broker._broker.Port):
+            return str(val)
+
 
 def to_json(val):
     """Convert broker types to JSON."""
